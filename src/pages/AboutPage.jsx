0c5d5857e5f1b2b0a0168909ce40448244db7980
miss-manisha-kapoor <manishakapoor209@gmail.com>
2023-08-21 13:39:20 +0530

    Shopping-Corner

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import women from '../assets/women.jpeg';
+import men from '../assets/men.jpeg';
+import jwelery from '../assets/jwelery.jpeg';
+import electronic from '../assets/electronic.jpeg';
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        We bring you the coolest and most innovative products that will make you the envy of all your friends! 
+
+We've got everything from tech gadgets that will blow your mind to health and fitness products that will help you get in the best shape of your life, and even household items that are so useful you'll wonder how you ever lived without them.
+
+<br/>So, what are you waiting for? Come check out our dropshipping store and see what all the fuss is about.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-10">
+              <img className="card-img-top " src={men} alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top " src={women} alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top " src={jwelery} alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top " src={electronic}alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
